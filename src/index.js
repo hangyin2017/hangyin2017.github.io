762f3c23b745bd04008ec97e86931638598f956a
Hang <hang.yin2017@outlook.com>
2020-09-15 17:46:43 +1000

    build: Introduced sass-loader.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './lib/bootstrap.min.css';
-import './index.css';
+import './scss/index.scss';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
       // {
       //   test: /\.(png|jpe?g|gif|svg)$/,
       //   use: ['file-loader'],
